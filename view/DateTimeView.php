4fb631a9f430415636a7458dcb05aeefe30bce72
JesperHolmstrom <jesper_jeppe@hotmail.com>
2015-09-10 16:22:56 +0200

    Date and time is presented

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,7 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$timeString = date('l') . ", the " . date('j') . 'th of ' . date('F') . " " . date('o') . ", The time is " . date('H:i:s'); //Save the current time to $timeString
 
 		return '<p>' . $timeString . '</p>';
 	}

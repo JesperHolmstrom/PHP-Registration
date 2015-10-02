55830ed9fc718092c48f7c629fa06346c0a0d691
JesperHolmstrom <jesper_jeppe@hotmail.com>
2015-10-02 21:02:51 +0200

    initial commit

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ date_default_timezone_set('Europe/Stockholm');
 
 //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 
-
 require_once('controller/PageController.php');
 $pageController = new PageController();
 $pageController->start();
